7b34cdeba2ef4a40b8002bbb0bf9c38f06ae0c23
陈国荣 <593283@qq.com>
2026-07-25 11:30:36 +0800

    feat: add resource attachment in create wizard + backend routes

--- cloud/src/modules/thing/handler/resources.rs
+++ cloud/src/modules/thing/handler/resources.rs
@@ -8,7 +8,10 @@ use axum::{
 use serde::Deserialize;
 use tinyiothub_web::response::ApiResponse;
 
-use super::super::{service::ThingService, types::ThingResource};
+use super::super::{
+    service::ThingService,
+    types::ThingResource,
+};
 use crate::shared::{api_response::ApiResponseBuilder, app_state::AppState};
 
 #[derive(Deserialize)]
